200aedf31893a5beb43126af2c4afa87bd7812e7
simanto97 <saimumsimanto97@gmail.com>
2023-08-13 13:30:08 +0600

    mongodb connection complete

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zunrmyl.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjphxo3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,21 +17,32 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-    const chemistryCollection = client.db("chemistryCorner").collection("users");
-
-
-
     // Send a ping to confirm a successful connection
+
+    const usersCollection = client.db("chemistryCorner").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send("user already exists");
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -41,9 +51,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("chemistry corner test runing");
-  });
-  
-  app.listen(port, () => {
-    console.log(`chemistry corner is runing on port ${port}`);
-  });
+  res.send("chemistry corner test runing");
+});
+
+app.listen(port, () => {
+  console.log(`chemistry corner is runing on port ${port}`);
+});
