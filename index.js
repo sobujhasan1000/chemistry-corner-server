e4c60d487be067e3931abf9179a0625fed336b05
mshipan <mshipan657@gmail.com>
2023-08-21 21:35:16 +0600

    single User get api created

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjphxo3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -151,6 +151,15 @@ async function run() {
         },
       };
       const result = await membersCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
+    // ======== get single member api =============
+
+    app.get("/member/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await membersCollection.findOne(query);
       res.send(result);
     });
 
