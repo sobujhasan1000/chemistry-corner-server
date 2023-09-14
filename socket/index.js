4cce2e35baccd799eaf506c15315ef161478ab93
simanto97 <saimumsimanto97@gmail.com>
2023-09-15 00:06:37 +0600

    app created

--- socket/index.js
+++ socket/index.js
@@ -43,6 +43,6 @@ app.get("/", (req, res) => {
   res.send("chemistry corner socket server is running");
 });
 
-app.listen(port, () => {
-  console.log(`chemistry corner socket server is running on port ☣️ ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`chemistry corner socket server is running on port ☣️ ${port}`);
+// });
